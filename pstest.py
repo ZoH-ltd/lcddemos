b0469f4fe16a54f88eb97f91d6c4772148643812
ZoH-ltd <zo-h@nifty.com>
2020-05-24 08:35:11 +0900

    表示整形部分を見直し。

--- pstest.py
+++ pstest.py
@@ -13,7 +13,9 @@ def get_net_bytes():
 	net_per = psutil.net_io_counters()
 	return net_per.bytes_sent + net_per.bytes_recv
 
-if __name__ == "__main__":
+def ps_watch():
+	""" psutil によりシステム状態を監視する。
+	"""
 	# 監視開始前のネットワーク使用量を取得。
 	net_byte_prev = get_net_bytes()
 
@@ -21,19 +23,39 @@ if __name__ == "__main__":
 		# 各種監視データを取得。
 		# ※CPU使用量の取得において、監視間隔も設定。
 		cpu_per  = psutil.cpu_percent(interval=WATCH_INTERVAL)
+		cpus_per = psutil.cpu_percent(interval=None, percpu=True)
 		cpu_freq = psutil.cpu_freq()
 		cpu_temp = psutil.sensors_temperatures()['cpu-thermal'][0].current
 		mem      = psutil.virtual_memory()
 		net_byte = get_net_bytes()
 
+		# 表示用の内容に整形。
+		# ○CPU:稼働率
+		cpus_disp = ''
+		for core_per in cpus_per:
+			if cpus_disp != '':
+				cpus_disp += ':'
+			cpus_disp += '{p:>3.0f}'.format(p = core_per)
+		cpu_disp  = '{:>5.1f}%'.format(cpu_per)
+
+		# ○CPU:動作クロック&温度
+		cpu_disp += '[' + cpus_disp + ']'
+		cpu_disp += ' ({f:>6.1f}MHz:{t:>5.1f}C)'.format(f = cpu_freq.current, t = cpu_temp)
+
+		# ○ネットワーク
+		n_val  = (net_byte - net_byte_prev) / (1024 * WATCH_INTERVAL / 8)
+		n_unit = "K"
+		if n_val > 1280:
+			n_val = n_val / 1024
+			n_unit = "M"
+		net_disp = '{nv:>7.2f}{nu}bps/sec'.format(nv = n_val, nu = n_unit)
+
 		# 取得したデータのうち、必要な内容を画面表示する。
-		line = 'CPU:{p:>5.1f}% ({f:>6.1f}MHz:{t:>5.1f}C),  MEM:{m:>5.1f}%,  NET:{n:>9.2f}KB/sec' \
+		line = 'CPU:{p},  MEM:{m:>5.1f}%,  NET:{n}' \
 		.format( \
-			p = cpu_per, \
-			f = cpu_freq.current, \
-			t = cpu_temp, \
+			p = cpu_disp, \
 			m = mem.percent, \
-			n = (net_byte - net_byte_prev) / (1024 * WATCH_INTERVAL) \
+			n = net_disp \
 		)
 #		print(line)
 		# 単純な print-line ではなく、表示行を上書きするように表示。
@@ -42,3 +64,6 @@ if __name__ == "__main__":
 
 		# 現在のネットワーク使用量を保存。
 		net_byte_prev = net_byte
+
+if __name__ == "__main__":
+	ps_watch()
